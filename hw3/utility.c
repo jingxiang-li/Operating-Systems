bf6309679a9105512002bcfddfc2ef014dd5c956
Jingxiang Li <lixx3899@umn.edu>
2015-11-03 18:28:29 -0600

    completes a single thread version function to process the queue

--- hw3/utility.c
+++ hw3/utility.c
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include "utility.h"
+
+static const int kBufferSize = 256;
 
 int change_wd (char *input_file_path) {
     char *input_path_dup, *wd;
@@ -22,3 +25,74 @@ int change_wd (char *input_file_path) {
     free(input_path_dup);
     return 0;
 }
+
+int compile_output(char *client_name, char *city_name, char *keywords) {
+    char output_path [kBufferSize];
+    sprintf(output_path, "./%s.result", client_name);
+
+    FILE *output_file = fopen(output_path, "w+");
+    if (NULL == output_file) {
+        fprintf(stderr, "Failed to create file %s\n", output_path);
+        perror(NULL);
+        return -1;
+    }
+
+    fprintf(output_file, "%s : %s\n", city_name, keywords);
+
+    fclose(output_file);
+    return 0;
+}
+
+void *process_queue(Queue *queue, Client_DB *client_db, DB *twitter_db,
+                    int thread_id, int *num_clients_left) {
+    while (num_clients_left && *num_clients_left != 0) {
+        // if there are clients waiting to be processed
+        if (queue == NULL || client_db == NULL || twitter_db == NULL) {
+            fprintf(stderr,
+                    "Failed to process the queue, at least one of queue, "
+                    "client_db, twitter_db is NULL pointer, illegal access\n");
+            return NULL;
+        }
+
+        int index, pop_result;
+
+        // CRITICAL SECTION
+        pop_result = pop(queue, &index);
+        (*num_clients_left)--;
+        // END OF CRITICAL SECTION
+
+        if (-1 == pop_result) {
+            fprintf(stderr, "Failed to pop element from the queue\n");
+            return NULL;
+        }
+
+        Client *client = get_client(client_db, index);
+        if (NULL == client) {
+            fprintf(stderr, "Failed to get client from the client_db\n");
+            return NULL;
+        }
+
+        char *client_name = client->name;
+        char *city_name = client->city_name;
+
+        printf("Thread %d is handling client %s\n", thread_id, client_name);
+
+        char *keywords = get_keywords(twitter_db, city_name);
+        if (NULL == keywords) {
+            fprintf(stderr, "Failed to match %s in the twitter database\n",
+                    city_name);
+            return NULL;
+        }
+
+        // compile the result file
+        if (-1 == compile_output(client_name, city_name, keywords)) {
+            fprintf(stderr, "Failed to compile the output\n");
+            return NULL;
+        }
+
+        printf("Thread %d finished handling client %s\n", thread_id,
+               client_name);
+    }
+    return NULL;
+}
+
--- hw3/utility.h
+++ hw3/utility.h
@@ -8,12 +8,37 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
+#include "./queue.h"
+#include "./client_db.h"
+#include "./twitter_db.h"
+
 /**
  * change the currect working directory to the directory holding
  * the input file
  * @param  input_file_path path to the input file
  * @return                 0 on success; otherwise -1
  */
-int change_wd (char *input_file_path);
+int change_wd(char *input_file_path);
 
+/**
+ * compile the output file for the client, named "client_name.result"
+ * @param  client_name name of the client (file)
+ * @param  city_name   name of the city
+ * @param  keywords    keywords
+ * @return             0 on success; -1 otherwise
+ */
+int compile_output(char *client_name, char *city_name, char *keywords);
+
+/**
+ * process the client queue using a single thread
+ * @param  queue            address of a queue instance
+ * @param  client_db        address of a Client_DB instance
+ * @param  twitter_db       address of a DB instance
+ * @param  thread_id        id of this thread
+ * @param  num_clients_left address to an integer for number of clients
+ * need to processed
+ * @return                  NULL
+ */
+void *process_queue(Queue *queue, Client_DB *client_db, DB *twitter_db,
+                    int thread_id, int *num_clients_left);
 #endif  // UTILITY_H
